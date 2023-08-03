0196e0cea30e0176d21087df454d0b32bcc46ac1
Kasutan <contact@kasutan.pro>
2023-08-03 20:22:55 +0200

    Template page simple

---
+++ template-simple.php
@@ -0,0 +1,16 @@
+<?php
+/*Template Name: Page simple*/
+
+
+
+
+// Bannière avec titre, sur-titre et décors
+add_action( 'tha_entry_top', 'kasutan_page_titre', 7 );
+
+
+// Build the page
+require get_template_directory() . '/index.php';
+
+
+
+
