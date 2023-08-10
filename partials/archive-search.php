c0a0857f9aa95a7308da01ffa4b64d6b2a0c9a8b
Kasutan <contact@kasutan.pro>
2023-08-10 20:23:20 +0200

    404 et résultats de recherche

--- partials/archive-search.php
+++ partials/archive-search.php
@@ -5,28 +5,39 @@
 **/
 
 $post_type=get_post_type();
-if($post_type==='product') {
-	$label_suite="Voir le produit";
+$tag='li';
+$post_id=get_the_ID();
+$titre=get_the_title($post_id);
+$link=get_the_permalink($post_id);
+
+if($post_type==='page') {
+	$label_suite="Lire la page";
 } else {
-	$label_suite="Lire la suite";
+	$label_suite="Lire l'article";
 }
-printf('<li class="post-summary %s">',$post_type);
 
-	ea_post_summary_image('woocommerce_thumbnail');
+printf('<%s class="vignette">',$tag);
+	if(function_exists('kasutan_affiche_image_vignette')) {
+		kasutan_affiche_image_vignette($post_id,$link);
+	}
 
-	echo '<div class="post-summary__content">';
-		ea_entry_category('archive');
-		if($post_type==='post') {
-			printf('<p class="entry-date">%s</p>',get_the_date('d/m/Y'));
-		}
-		
-		ea_post_summary_title();
-		the_excerpt();
-		if($post_type==='product') {
-			global $product;
-			printf('<p class="price">%s</p>', $product->get_price_html());
-		} 
-		printf('<a href="%s" class="suite">%s<span class="screen-reader-text">de %s</span><span class="chevrons-suite">>>></span></a>',get_the_permalink(),$label_suite,get_the_title());
+	echo '<div class="texte">';
+
+		$date=get_the_date('',$post_id);
+		printf('<p class="date">%s</p>',$date);
+
+		printf('<h2 class="titre-item"><a href="%s">%s</a></h2>',$link,$titre);
+			
+		printf('<a class="extrait" href="%s">%s</a>',$link,get_the_excerpt($post_id));
+
+		printf('<div class="suite"><a href="%s">%s<span class="screen-reader-text"> %s</span></a></div>',$link,$label_suite,$titre);
 	echo '</div>';
 
-echo '</li>';
+	/**Pour vignette en position top post */
+	printf('<h2 class="titre-item top-post"><a href="%s">%s</a></h2>',$link,$titre);
+			
+	printf('<a class="extrait top-post" href="%s">%s</a>',$link,get_the_excerpt($post_id));
+
+	printf('<div class="suite top-post"><a href="%s">%s<span class="screen-reader-text"> %s</span></a></div>',$link,$label_suite,$titre);
+	
+printf('</%s>',$tag);
