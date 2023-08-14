bbdba2dab3062fc2cd83c948898039c3e97b7855
Kasutan <contact@kasutan.pro>
2023-08-14 16:57:51 +0200

    Pas d'image à la une pour les pages (confusion avec bannière)

--- functions.php
+++ functions.php
@@ -58,7 +58,7 @@ if ( ! function_exists( 'kasutan_setup' ) ) :
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails', array('post','page','references'));
+		add_theme_support( 'post-thumbnails', array('post'));
 
 		register_nav_menus( array(
 			'primary' => 'Menu principal',
