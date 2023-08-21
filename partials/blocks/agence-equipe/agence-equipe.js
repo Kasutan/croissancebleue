d9a7e22b520ee4c9ba739e3441427236f5350990
Kasutan <contact@kasutan.pro>
2023-08-21 12:16:18 +0200

    Agence équipe : logo bleu et nom cliquable

--- partials/blocks/agence-equipe/agence-equipe.js
+++ partials/blocks/agence-equipe/agence-equipe.js
@@ -5,6 +5,7 @@
 	$( document ).ready(function() {
 		$('.equipier .toggle').click(function (e) { 
 			var volet=$('#'+$(this).attr('aria-controls'));
+			var sibling=$(this).siblings('button'); //l'autre bouton
 			if($(this).attr('aria-expanded')=="false"){
 				//On ferme tous les volets
 				$('.equipier .toggle').attr('aria-expanded','false');
@@ -12,9 +13,11 @@
 				
 				//On ouvre celui-ci
 				$(this).attr('aria-expanded','true');
+				$(sibling).attr('aria-expanded','true'); //mettre en cohérence l'autre bouton
 				$(volet).slideDown('slow');
 			} else {
 				$(this).attr('aria-expanded','false');
+				$(sibling).attr('aria-expanded','false'); //mettre en cohérence l'autre bouton
 				$(volet).slideUp('slow');
 			}
 			
